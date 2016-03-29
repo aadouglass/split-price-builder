1b844be352448b309eb2716e7f54a60b555afaa3
aadouglass <aadouglass.dt@gmail.com>
2016-03-29 14:29:32 -0500

    Updated formatting

--- split-price-builder.sh
+++ split-price-builder.sh
@@ -4,5 +4,5 @@
 data_file=$1                                               # data_file is arg1
 while IFS=$',' read -a my_array || [[ -n "$my_array" ]];   # Create an array with each field of current line
 do
-	echo ${my_array[@]}                                    # Write out all elements of the array
+	echo ${my_array[@]}                                 	   # Write out all elements of the array
 done < $data_file                                          # Done using $data_file
