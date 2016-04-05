e8a7f44365633e8400b5fa258020a6f5bac49701
aadouglass <aadouglass.dt@gmail.com>
2016-04-05 14:22:39 -0500

    WIP for array appending

--- split-price-builder.sh
+++ split-price-builder.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-	                          
+
 data_file=$1                                               # data_file is arg1
+line_splitter() {																					 # Create function
 while IFS=$',' read -a my_array || [[ -n "$my_array" ]];   # Create an array with each field of current line
 do
-	echo ${my_array[@]}                                 	   # Write out all elements of the array
+	echo ${my_array[*]}                                   	 # Write out all elements of the array
 done < $data_file                                          # Done using $data_file
+}																													 # Close/End function
+line_splitter																							 # Call function
