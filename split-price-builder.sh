5ccd5ee705c24d2eae3ab3ba2b61d13a37ccf187
aadouglass <aadouglass.dt@gmail.com>
2016-04-14 10:58:22 -0500

    added debug function and edited format of script slightly

--- split-price-builder.sh
+++ split-price-builder.sh
@@ -6,6 +6,8 @@ data_file=$1
 size_line=$2
 # select_fields is argument 3, from end of line what is the range of fields of prices?
 select_fields=$3
+# debug variable flag
+debug=$4
 # Clean the variable with sizes to clear white space
 trim_sizes() {
 	temp_sizes1=$(sed -n "${size_line}p" "$data_file")
@@ -23,7 +25,7 @@ split_lines() {
 	do
 		row_array[$i]="${my_array[@]}"
 		# print value to file for checking
-		printf %s "${row_array[*]}" > 'input-file-rows.txt'
+		printf %s "${row_array[*]}" > 'tmp/input-file-rows.txt'
 		i=`expr $i + 1`
 		# finished using data_file		
 	done < $data_file
@@ -37,7 +39,7 @@ get_sizes() {
 	size_array=()
 	IFS=',' read -a size_array <<< $sizes
 	# print value to file for checking
-	printf %s "${size_array[*]}" > 'input-file-sizes.txt'
+	printf %s "${size_array[*]}" > 'tmp/input-file-sizes.txt'
 }
 
 
@@ -46,69 +48,57 @@ get_sizes() {
 get_parts() {
 	temp_part=()
 	parts_array=()
-	f1() {
 	temp_part=$(cut -d',' -f1 $data_file)
 	# print value to file for checking
-	printf %s "${temp_part[*]}" > 'input-file-parts.txt'
-	}
-	f1
-	sed -i -e '$a\' 'input-file-parts.txt'
+	printf %s "${temp_part[*]}" > 'tmp/input-file-parts.txt'
+	sed -i -e '$a\' 'tmp/input-file-parts.txt'
 	i=0
-	f2() {
 	while IFS='\n' read -a another_array
 	do
 		parts_array[$i]="${another_array[@]}"
-		printf %s "${parts_array[*]}" > 'output-file-parts.txt'
+		printf %s "${parts_array[*]}" > 'tmp/output-file-parts.txt'
 		i=`expr $i + 1`
-	done < input-file-parts.txt
-	}
-	f2
+	done < tmp/input-file-parts.txt
 }
 
 
 # create array part_perms to store permutations of part numbers with sizes
 # create function that will concatenate part numbers with all sizes
 permutation_creation() {
-	permutations() {
-		# ensure file is empty before output is created
-		> permutations.txt	
-		part_perms=()
-		i=1
-		l=0
-		pa=`expr ${#parts_array[@]}`
-		sa=${#size_array[@]}
-		test_amt=`expr $pa \* $sa`
-		while [ $i -lt $pa ]
+	# ensure file is empty before output is created
+	> tmp/permutations.txt	
+	part_perms=()
+	i=1
+	l=0
+	pa=`expr ${#parts_array[@]}`
+	sa=${#size_array[@]}
+	test_amt=`expr $pa \* $sa`
+	while [ $i -lt $pa ]
+	do
+		n=0
+		while [ $n -lt ${#size_array[@]} ]
 		do
-			n=0
-			while [ $n -lt ${#size_array[@]} ]
-			do
-				f=`expr "${#part_perms[@]}" + 1`
-				part="${parts_array[$i]}-"
-				size="${size_array[$n]}"
-				part_perms[$f]=${part}${size}
-				n=`expr $n + 1`			
-				l=`expr $l + 1`
-				# Append to file of permuatations as to not overwrite its contents
-				printf "${part_perms[$f]}\n" >> 'permutations.txt'
-			done
-			i=`expr $i + 1`
+			f=`expr "${#part_perms[@]}" + 1`
+			part="${parts_array[$i]}-"
+			size="${size_array[$n]}"
+			part_perms[$f]=${part}${size}
+			n=`expr $n + 1`			
+			l=`expr $l + 1`
+			# Append to file of permuatations as to not overwrite its contents
+			printf "${part_perms[$f]}\n" >> 'tmp/permutations.txt'
 		done
-	}
-	permutations
-	create_final_part_array() {
-		i=0
-		final_part_array=()
-		while IFS='' read -a my_array
-		do
-			final_part_array[$i]="${my_array[@]}"
-			# print value to file for checking
-			i=`expr $i + 1`
-			# finished using permutations.txt		
-		done < permutations.txt
-		printf %s "${final_part_array[*]}" > 'final_part.txt'
-	}
-	create_final_part_array
+		i=`expr $i + 1`
+	done
+	i=0
+	final_part_array=()
+	while IFS='' read -a my_array
+	do
+		final_part_array[$i]="${my_array[@]}"
+		# print value to file for checking
+		i=`expr $i + 1`
+		# finished using tmp/permutations.txt		
+	done < tmp/permutations.txt
+	printf %s "${final_part_array[*]}" > 'tmp/final_part.txt'
 }
 
 
@@ -119,63 +109,54 @@ select_prices() {
 		while read
 			do
 			temp_array[$i]=$(sed 's/,*$//' | rev | cut -d ',' -f $select_fields | rev)		
-			echo "${temp_array[$i]}" > 'price-file.txt'
+			echo "${temp_array[$i]}" > 'tmp/price-file.txt'
 			i=`expr $i + 1`
 		done < $data_file	
 }
 
 
 split_price() {
-	# the first function splits the lines of prices into an array
-	first() {
-		prices=()
-		i=1
-		pa=`expr ${#parts_array[@]}`
-		while [ $i -lt $pa ]
-		do
-			line=$(sed -n "$i{p}" < price-file.txt)
-			prices[$i]=$line
-			i=`expr $i + 1`
-		done
-		printf '%s\n' "${prices[@]}" > prices.txt
-	}
-	first
-	# the second function will split each line of prices into individual elements of a new array
-	second() {
-		> prices.txt
-		fin_price=()
-		i=0
-		z=1
-		pa=`expr ${#parts_array[@]}`
-		while [ $i -lt $pa ]
-		do
-			line=$(sed -n "$z{p}" < price-file.txt)
-			n=0
-			IFS=',' read -r -a array <<< $line
-			while [ $n -lt ${#size_array[@]} ]
-			do
-				echo "${array[$n]}" >> prices.txt
-				n=`expr $n + 1`
-			done
+	prices=()
+	i=1
+	pa=`expr ${#parts_array[@]}`
+	while [ $i -lt $pa ]
+	do
+		line=$(sed -n "$i{p}" < tmp/price-file.txt)
+		prices[$i]=$line
 		i=`expr $i + 1`
-		z=`expr $z + 1`
-		done
-		removing_newlines=$(<prices.txt); printf '%s\n' "$removing_newlines" > prices.txt
-	}
-	second
-	create_final_price_array() {
-		final_price_array=()
-		i=0
-		while IFS='' read -a my_array
+	done
+	printf '%s\n' "${prices[@]}" > tmp/prices.txt
+	> tmp/prices.txt
+	fin_price=()
+	i=0
+	z=1
+	pa=`expr ${#parts_array[@]}`
+	while [ $i -lt $pa ]
+	do
+		line=$(sed -n "$z{p}" < tmp/price-file.txt)
+		n=0
+		IFS=',' read -r -a array <<< $line
+		while [ $n -lt ${#size_array[@]} ]
 		do
-			final_price_array[$i]="${my_array[@]}"
-			i=`expr $i + 1`
-			# finished using permutations.txt		
-		done < prices.txt
-		printf %s "${final_price_array[*]}" > 'final_price.txt'
-	}
-	create_final_price_array
+			echo "${array[$n]}" >> tmp/prices.txt
+			n=`expr $n + 1`
+		done
+	i=`expr $i + 1`
+	z=`expr $z + 1`
+	done
+	removing_newlines=$(<tmp/prices.txt); printf '%s\n' "$removing_newlines" > tmp/prices.txt
+	final_price_array=()
+	i=0
+	while IFS='' read -a my_array
+	do
+		final_price_array[$i]="${my_array[@]}"
+		i=`expr $i + 1`
+		# finished using tmp/permutations.txt		
+	done < tmp/prices.txt
+	printf %s "${final_price_array[*]}" > 'tmp/final_price.txt'
 }
+
+
 # print part number next to price
 final_output() {
 	> final_output.txt
@@ -183,9 +164,18 @@ final_output() {
 	i=0
 	while [ $i -lt $length ]
 	do
-		printf '%s %s\n' "${final_part_array[$i]}" "${final_price_array[$i]}" >> final_output.txt
+		printf "${final_part_array[$i]},${final_price_array[$i]}\n" >> final_output.txt
 		i=`expr $i + 1`
 	done
+	xdg-open final_output.txt
+}
+
+
+remove_temp_files() {
+	if [ "$debug" -eq 1 ]
+		then
+		rm tmp/*
+	fi
 }
 
 
@@ -200,10 +190,9 @@ main() {
 	select_prices
 	split_price
 	final_output
+	remove_temp_files
 }
 main
-# open file at end of script to view output
-xdg-open final_output.txt
 
 
 
