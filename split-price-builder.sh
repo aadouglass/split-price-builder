ab187c6a57df532f40b6d06bea99d74db0080220
aadouglass <aadouglass.dt@gmail.com>
2016-04-15 15:39:46 -0500

    updated documentation

--- split-price-builder.sh
+++ split-price-builder.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# data_file is argument 1
+# give me the path to the data
 data_file=$1
-# size_line is argument 2, what line are the sizes located?
+# what line are the sizes located?
 size_line=$2
-# select_fields is argument 3, from end of line what is the range of fields of prices?
+# from end of line what is the range of fields of prices?
 select_fields=$3
-# debug variable flag
+# if you want to clear temp files created in script, pass '1'
+# if you want to see the output of the script, pass '2'
 debug=$4
+# a simple variable denoting temporary directory
+t="$t"
+
+
 # Clean the variable with sizes to clear white space
 trim_sizes() {
 	temp_sizes1=$(sed -n "${size_line}p" "$data_file")
@@ -16,64 +21,61 @@ trim_sizes() {
 }
 
 
-# Initiate row_array to store each row from argument 1 data_file using the while loop
-# Splits each row of a document into an element of row_array
+# Splits each row of a document into an element of row_array for easier manipulation
 split_lines() {
 	i=0
 	row_array=()
 	while IFS='' read -a my_array
 	do
 		row_array[$i]="${my_array[@]}"
-		# print value to file for checking
-		printf %s "${row_array[*]}" > 'tmp/input-file-rows.txt'
+		# print value to file for checking if errors exist
+		printf %s "${row_array[*]}" > "$t"input-file-rows.txt
 		i=`expr $i + 1`
 		# finished using data_file		
 	done < $data_file
 }
 
 
-# Initiate size_array to store sizes from the an element of row_array
-# Split element with sizes into a new array by passing the row that sizes exist in the document
-# Sizes will be used to create SKU permutations in later function
+
+# Sizes are extracted to create SKU permutations in later function
 get_sizes() {
 	size_array=()
 	IFS=',' read -a size_array <<< $sizes
-	# print value to file for checking
-	printf %s "${size_array[*]}" > 'tmp/input-file-sizes.txt'
+	# print value to file for checking if errors exist
+	printf %s "${size_array[*]}" > "$t"input-file-sizes.txt
 }
 
 
-# create parts_array to store all part numbers for creating part number permutations
 # Extract all part numbers from given file to later create permutations of the numbers with sizes
 get_parts() {
 	temp_part=()
 	parts_array=()
+	# 
 	temp_part=$(cut -d',' -f1 $data_file)
-	# print value to file for checking
-	printf %s "${temp_part[*]}" > 'tmp/input-file-parts.txt'
-	sed -i -e '$a\' 'tmp/input-file-parts.txt'
+	# print value to file for checking if errors exist and to manipulate data further
+	printf %s "${temp_part[*]}" > "$t"input-file-parts.txt
+	sed -i -e '$a\' "$t"input-file-parts.txt
 	i=0
 	while IFS='\n' read -a another_array
 	do
 		parts_array[$i]="${another_array[@]}"
-		printf %s "${parts_array[*]}" > 'tmp/output-file-parts.txt'
+		printf %s "${parts_array[*]}" > "$t"output-file-parts.txt
 		i=`expr $i + 1`
-	done < tmp/input-file-parts.txt
+	done < "$t"input-file-parts.txt
 }
 
 
-# create array part_perms to store permutations of part numbers with sizes
-# create function that will concatenate part numbers with all sizes
+# concatenate part numbers with all sizes to be matched with prices later
 permutation_creation() {
 	# ensure file is empty before output is created
-	> tmp/permutations.txt	
+	> "$t"permutations.txt	
 	part_perms=()
 	i=1
 	l=0
-	pa=`expr ${#parts_array[@]}`
+	parts_array_length=`expr ${#parts_array[@]}`
 	sa=${#size_array[@]}
-	test_amt=`expr $pa \* $sa`
-	while [ $i -lt $pa ]
+	test_amt=`expr $parts_array_length \* $sa`
+	while [ $i -lt $parts_array_length ]
 	do
 		n=0
 		while [ $n -lt ${#size_array[@]} ]
@@ -85,7 +87,7 @@ permutation_creation() {
 			n=`expr $n + 1`			
 			l=`expr $l + 1`
 			# Append to file of permuatations as to not overwrite its contents
-			printf "${part_perms[$f]}\n" >> 'tmp/permutations.txt'
+			printf "${part_perms[$f]}\n" >> "$t"permutations.txt
 		done
 		i=`expr $i + 1`
 	done
@@ -94,66 +96,68 @@ permutation_creation() {
 	while IFS='' read -a my_array
 	do
 		final_part_array[$i]="${my_array[@]}"
-		# print value to file for checking
+		# print value to file for checking if errors exist
 		i=`expr $i + 1`
-		# finished using tmp/permutations.txt		
-	done < tmp/permutations.txt
-	printf %s "${final_part_array[*]}" > 'tmp/final_part.txt'
+	done < "$t"permutations.txt
+	printf %s "${final_part_array[*]}" > "$t"final_part.txt
 }
 
 
-# select_fields is the third argument, count starting at 1 from the end of line end on last price count
+
+# select_prices function stores prices from the original data file into a new file for manipulation
 select_prices() {
-	# select_prices function stores prices from the original data file into a new file for manipulation
-		i=0
-		while read
-			do
+	i=0
+	while read
+		do
+			# select_fields is the third argument, count starting at 1 from the end of line (last price) to first price (somewhere in the middle of the line)
 			temp_array[$i]=$(sed 's/,*$//' | rev | cut -d ',' -f $select_fields | rev)		
-			echo "${temp_array[$i]}" > 'tmp/price-file.txt'
+			echo "${temp_array[$i]}" > "$t"price-file.txt
 			i=`expr $i + 1`
-		done < $data_file	
+	done < $data_file	
 }
 
 
 split_price() {
 	prices=()
 	i=1
-	pa=`expr ${#parts_array[@]}`
-	while [ $i -lt $pa ]
+	parts_array_length=`expr ${#parts_array[@]}`
+	# set line
+	# create array of prices to later be added into final txt doc with corresponding part permutation
+	while [ $i -lt $parts_array_length ]
 	do
-		line=$(sed -n "$i{p}" < tmp/price-file.txt)
-		prices[$i]=$line
+		prices[$i]=$(sed -n "$i{p}" < "$t"price-file.txt)
 		i=`expr $i + 1`
 	done
-	printf '%s\n' "${prices[@]}" > tmp/prices.txt
-	> tmp/prices.txt
+	> "$t"prices.txt
 	fin_price=()
 	i=0
 	z=1
-	pa=`expr ${#parts_array[@]}`
-	while [ $i -lt $pa ]
+	parts_array_length=`expr ${#parts_array[@]}`
+	# split each comma delimited line into newline on each comma to be stored into individual array elements for matching with part permutation
+	while [ $i -lt $parts_array_length ]
 	do
-		line=$(sed -n "$z{p}" < tmp/price-file.txt)
+		line=$(sed -n "$z{p}" < "$t"price-file.txt)
 		n=0
 		IFS=',' read -r -a array <<< $line
 		while [ $n -lt ${#size_array[@]} ]
 		do
-			echo "${array[$n]}" >> tmp/prices.txt
+			echo "${array[$n]}" >> "$t"prices.txt
 			n=`expr $n + 1`
 		done
 	i=`expr $i + 1`
 	z=`expr $z + 1`
 	done
-	removing_newlines=$(<tmp/prices.txt); printf '%s\n' "$removing_newlines" > tmp/prices.txt
+	removing_newlines_from_end_of_doc=$(<"$t"prices.txt); printf '%s\n' "$removing_newlines_from_end_of_doc" > "$t"prices.txt
 	final_price_array=()
 	i=0
+	# storing each line of now individual prices into an array for easy matching with corresponding part permutation
 	while IFS='' read -a my_array
 	do
 		final_price_array[$i]="${my_array[@]}"
 		i=`expr $i + 1`
-		# finished using tmp/permutations.txt		
-	done < tmp/prices.txt
-	printf %s "${final_price_array[*]}" > 'tmp/final_price.txt'
+		# finished using "$t"permutations.txt		
+	done < "$t"prices.txt
+	printf %s "${final_price_array[*]}" > "$t"final_price.txt
 }
 
 
@@ -167,14 +171,24 @@ final_output() {
 		printf "${final_part_array[$i]},${final_price_array[$i]}\n" >> final_output.txt
 		i=`expr $i + 1`
 	done
-	xdg-open final_output.txt
+	echo "Data is stored in tmp/final_output.txt"
 }
 
 
-remove_temp_files() {
-	if [ "$debug" -eq 1 ]
+
+# when debug equals 1, all of the files in the temp directory will be cleared
+# when debug equals 2, default text editor will open the output of the script 
+debug() {
+	if [ -z "$debug" ]; then
+		exit
+	fi
+	if [[ "$debug" -eq 1 ]]
 		then
-		rm tmp/*
+		rm "$t"*
+	fi
+	if [[ "$debug" -eq 2 ]]
+		then
+		xdg-open final_output.txt
 	fi
 }
 
@@ -190,10 +204,6 @@ main() {
 	select_prices
 	split_price
 	final_output
-	remove_temp_files
+	debug
 }
 main
-
-
-
-
