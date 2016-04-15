2332b1e3827f597cfb34837df870b3d53e233d56
aadouglass <aadouglass.dt@gmail.com>
2016-04-15 11:38:43 -0500

    work in progress exploder

--- sploder.sh
+++ sploder.sh
@@ -8,43 +8,13 @@ last_element_index=$((${#range_array[@]}-1))
 begin_range="${range_array[0]}"
 end_range="${range_array[$last_element_index]}"
 
-size_line=$(head -n 1 $data_file)
 
-IFS=',' read -a size_array <<< $size_line
 
-#l=0
-#while [ "$l" -lt "${#size_array[@]}" ]
-#do
-#	i=0
-#	while [ "$i" -lt "${#ranges_array[@]}" ]
-#	do
-#		if [ "${size_array[$l]%-*}" = "${ranges_array[$i]}" ]
-#		then
-#			temp_var=${size_array[$l]%-*}
-#		fi
-#	
-#		t=0
-#		if [[ "${size_array[$l]}" = *-* ]]
-#		then
-#			while [ "$t" -lt "${#ranges_array[@]}" ]	
-#			do
-#				if [ "${size_array[$t]#*-}" = "${ranges_array[$i]}" ]
-#				then
-#					tmp_var=${size_array[$l]#*-}
-#					echo "$temp_var-$tmp_var"
-#				fi
-#				t=`expr $t + 1`
-#			done
-#	fi
-#		
-#		
-#		#ele_range_end=${a#*-}
-#		#ele_range_beg=${a%-*}
-#
-#		i=`expr $i + 1`
-#	done
-#	l=`expr $l + 1`
-#done
+trim_format() {
+	header=$(head -n 1 $data_file)
+	trimmed=$(sed 's/\//-/g' <<< $header)
+}
+
 
 
 get_index() {
@@ -56,46 +26,47 @@ get_index() {
 	   fi
 	done
 }
-array=(2XL 3XL 4XL)
-
-
-var=$(get_index "${array[1]}")
 
 
 
-
-
-
-
-
-
-function1() {
+explode_sizes() {
+	size_line=$(head -n 1 $data_file)
+	IFS=',' read -a size_array <<< $trimmed
 	i=0
-	> tmp.txt
-	while [ "$i" -lt "${#size_array[@]}" ]; do
-		n=$(($i+1))
-		ele_split=(${size_array[$i]%-*} ${size_array[$i]#*-})
-		
+	> tmp/tmp.txt
+	while [ "$i" -lt "${#size_array[@]}" ]; do		
 		if [[ "${size_array[$i]}" = *-* ]]; then
+			ele_split[0]=$(cut -d - -f 1 <<< "${size_array[$i]}")
+			ele_split[1]=$(<<< "${size_array[$i]}" rev | cut -d - -f 1 | rev )	
 			declare -i tmp0=$(get_index "${ele_split[0]}")
-			declare -i tmp1=$(get_index "${ele_split[1]}")
+			declare -i tmp1=$(get_index "${ele_split[1]}")			
 			dif=$(($tmp1 - $tmp0))
 			if [[ "$dif" -lt 2 ]]; then
-				printf "${size_array[$i]%-*},${size_array[$i]#*-}," >> tmp.txt
+				printf "${ele_split[0]},${ele_split[1]}," >> tmp/tmp.txt
 			else
-				tmp2=$((tmp1+1))
-				while [ "$tmp0" -lt "$tmp2" ]; do
-					printf "${range_array[$tmp0]}," >> tmp.txt
-					tmp0=$(($tmp0+1))
+				while [ "$tmp0" -le "$tmp1" ]; do
+					printf "${range_array[$tmp0]}," >> tmp/tmp.txt
+					tmp0=$(($tmp0 + 1))
 				done
 			fi
 		else
-			printf "${size_array[$i]%-*}," >> tmp.txt			
+			printf "${size_array[$i]%-*}," >> tmp/tmp.txt			
 		fi
 		i=$(($i + 1))
 	done
-	xdg-open tmp.txt
+	> tmp/tmpdoc.txt
+	sed -n 's/,*$//p' tmp/tmp.txt >> tmp/tmpdoc.txt
+	xdg-open tmp/tmpdoc.txt
 }
-function1
 
 
+
+
+
+
+
+main() {
+	trim_format
+	explode_sizes
+}
+main
